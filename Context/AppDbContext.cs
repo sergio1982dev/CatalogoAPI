736fd413cedccac62557fdccc500786a243c8c84
sergio1982dev <sergio1982.dev@gmail.com>
2024-02-05 12:00:07 -0300

    criados modelos de dominio, classe de contexto e populadas as tabelas

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CatalogoAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogoAPI.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        { }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
