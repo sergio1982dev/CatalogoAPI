32c9519bad77a245fe47a39e107321d820a67c2a
sergio1982dev <sergio1982.dev@gmail.com>
2024-02-05 18:27:47 -0300

    criados controladores, realizados ajustes e otimizacao na serializacao Json e tratados erros com try-catch

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace CatalogoAPI.Models
 {
@@ -24,6 +25,8 @@ namespace CatalogoAPI.Models
         public DateTime DataCadastro { get; set; }
 
         public int CategoriaId { get; set; }
+
+        [JsonIgnore]
         public Categoria? Categoria { get; set; }
     }
 }
